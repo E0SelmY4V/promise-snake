a3a608c56a832993a9ff5f0547737b814b7a43e4
E0SelmY4V-PC <2507917746@qq.com>
2023-03-11 21:11:03 +0800

    Add 注释

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,37 @@
 import '';
 
+/**
+ * Continuous asynchronous operation for ES6 Promise
+ * @module promise-snake
+ * @license MIT
+ * @link https://github.com/E0SelmY4V/promise-snake
+ */
+declare module 'promise-snake';
+
 declare global {
 	interface Promise<T> {
+		/**
+		 * Create another Promise when this Promise has been resolved.
+		 * @param executor The callback used to initialize 'another Promise'.
+		 */
 		next<T>(executor: Promise.Executor<T>): Promise<T>;
 	}
 	interface PromiseConstructor {
+		/**
+		 * Create promises one by one to do asynchronous operations.
+		 * @param executors Callbacks used to initialize the promises.
+		 */
 		snake(executors: readonly Promise.Executor<void>[]): Promise<void>;
 	}
 	namespace Promise {
+		/**
+		 * The `executor` argument of {@link PromiseConstructor}.
+		 *
+		 * Callbacks used to initialize the promise. This type of callback is
+		 * passed two arguments: a resolve callback used to resolve the promise
+		 * with a value or the result of another promise, and a reject callback
+		 * used to reject the promise with a provided reason or error.
+		 */
 		type Executor<T> = (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void;
 	}
 }
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Continuous asynchronous operation for ES6 Promise
+ * @module promise-snake
+ * @license MIT
+ * @link https://github.com/E0SelmY4V/promise-snake
+ */
+"use strict";
+
 Promise.prototype.next = function (executor) {
 	return this.then(() => new Promise(executor));
 };
